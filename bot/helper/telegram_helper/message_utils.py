6f5b1e93b7506865ced6f8e375fab2782b76439e
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-02-26 18:40:27 +0700

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -67,6 +67,8 @@ def update_all_messages():
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
+                if len(msg) == 0:
+                    msg = "Starting DL"
                 try:
                     editMessage(msg, status_reply_dict[chat_id])
                 except Exception as e:
@@ -82,6 +84,8 @@ def sendStatusMessage(msg, bot):
                 message = status_reply_dict[msg.message.chat.id]
                 deleteMessage(bot, message)
                 del status_reply_dict[msg.message.chat.id]
+        if len(progress) == 0:
+            progress = "Starting DL"
             except Exception as e:
                 LOGGER.error(str(e))
                 del status_reply_dict[msg.message.chat.id]
